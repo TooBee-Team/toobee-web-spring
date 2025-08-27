a6f33484c07896402bbab36eb9fba11bbcfc69af
Fungus <jkfungus@yandex.com>
2025-08-28 00:01:21 +0800

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,13 @@ import org.springframework.boot.gradle.tasks.run.BootRun
 
 plugins {
 	java
-	id("org.springframework.boot") version "3.5.0"
+	id("org.springframework.boot") version "3.5.5"
 	id("io.spring.dependency-management") version "1.1.7"
 	id("org.graalvm.buildtools.native") version "0.10.6"
 }
 
 group = "top.toobee"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.1"
 
 java {
 	toolchain {
@@ -21,16 +21,16 @@ repositories {
 }
 
 dependencies {
-	setOf("web", "security", "jdbc", "websocket", "oauth2-client", "oauth2-authorization-server").forEach {
+	setOf("web", "security", "jdbc", "websocket", "data-jpa").forEach {
 		implementation("org.springframework.boot:spring-boot-starter-$it")
 	}
+	//implementation("org.apache.commons:commons-lang3:3.17.0")
+	implementation("it.unimi.dsi:fastutil:8.5.15")
 	runtimeOnly("org.postgresql:postgresql")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-	//implementation("org.apache.commons:commons-lang3:3.17.0")
-	implementation("it.unimi.dsi:fastutil:8.5.15")
 }
 
 /*
